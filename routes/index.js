9dc423f2010c2dbb52fc5c79e330341996c55fd2
Nic Ledoux <NL15025100@gmail.com>
2022-08-26 22:09:24 -0700

    Updated styles and hbs files

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 // @des     Login/Landing page
 // @route   GET /
 router.get("/", (req, res) => {
-  res.render("login");
+  res.render("login", {
+    layout: "login",
+  });
 });
 
 // @des     Dashboard
